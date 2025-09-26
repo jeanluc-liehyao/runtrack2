4103f3a42e505e3297412337002ae6a443c55ce4
unknown <jean-luc.lieh-yao@laplateforme.io>
2025-09-26 11:03:10 +0200

    job 1 fini

---
+++ jour1/job1/index.php
@@ -0,0 +1,22 @@
+<?php
+$str="La Plateforme";
+$str2="Vive";
+$str3="!";
+
+echo $str2. " " .$str." ". $str3 ."<br>";
+
+$val= 6;
+$val2= $val+4;
+
+echo $val ." et ". $val2 ."<br>";
+
+$myBool= true;
+
+echo ($myBool? 'true' : 'false'). "<br>";
+
+$myBool= false;
+
+echo ($myBool? 'true' : 'false'). "<br>";
+
+
+?>
