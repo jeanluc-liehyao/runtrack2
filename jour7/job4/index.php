506b6a8c2502d47d08416955c6c70ad77a3ed3c7
unknown <jean-luc.lieh-yao@laplateforme.io>
2025-10-16 11:20:20 +0200

    job 4 fini

---
+++ jour7/job4/index.php
@@ -0,0 +1,26 @@
+<?php
+function calcule($a, $operation, $b) {
+    switch ($operation) {
+        case '+':
+            return $a + $b;
+        case '-':
+            return $a - $b;
+        case '*':
+            return $a * $b;
+        case '/':
+            if ($b == 0) {
+                return "IMPOSSIBLE : division par zéro";
+            }
+            return $a / $b;
+        case '%':
+            if ($b == 0) {
+                return "IMPOSSIBLE : modulo par zéro";
+            }
+            return $a % $b;
+        default:
+            return "Opération non reconnue";
+    }
+}
+
+echo calcule(1,"+",1);
+?>
