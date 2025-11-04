89bc6a47df8e823c9279aeba16c03aebf7e0a0dc
unknown <jean-luc.lieh-yao@laplateforme.io>
2025-11-04 09:41:42 +0100

    job 5 fini

---
+++ jour10/job5/index.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "jour9";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM etudiants WHERE TIMESTAMPDIFF(YEAR, naissance, CURDATE()) < 18";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1' cellpadding='8' cellspacing='0'>";
+    echo "<tr>";
+    while ($fieldinfo = $result->fetch_field()) {
+        echo "<th>" . htmlspecialchars($fieldinfo->name) . "</th>";
+    }
+    echo "</tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Aucun étudiant de moins de 18 ans trouvé.";
+}
+
+$conn->close();
+?>
